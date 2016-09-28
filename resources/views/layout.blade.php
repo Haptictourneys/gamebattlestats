7ff3c57571bb6c66c4c25b1c024e3a82f72058b4
voodoodev <art4fusion@gmail.com>
2016-09-28 08:46:43 +0300

    first

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>GameBattleStats</title>
+		<link rel="stylesheet" type="text/css" href="templates/gbs/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="templates/gbs/css/main.css">
+
+    	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script> 
+		<script type="text/javascript" src="templates/gbs/js/bootstrap.min.js"></script>
+	</head>
+	<body>
+		@yield('header')
+
+		@yield('content')
+
+		@section('footer')
+	</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,69 @@
+@extends('layout')
+
+@section('header')
+    <div class="container search-nav">
+        <div class="col-md-10">
+        </div>
+        <div class="col-md-2">
+            <a href="#">
+                <span class="glyphicon glyphicon-search"></span>
+            </a>
+            <a href="#">
+                <span class="glyphicon glyphicon-user"></span>
+            </a>
+        </div>
+    </div>
+
+    <nav class="navbar header-bg">
+        <div class="container">
+            <div class="navbar-header col-md-4">
+                <a href="#"><img src="templates/gbs/images/logo.png" alt="" class="img-responsive logo"></a>
+            </div>
+
+            <div class="col-md-6 col-md-offset-2">
+                <ul class="nav navbar-nav text-center">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="#">Tournaments</a></li>
+                    <li><a href="#">Ladders</a></li>
+                    <li><a href="#">FAQ</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container">
+        <div id="home-carousel" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#home-carousel" data-slide-to="0" class="active"></li>
+                <li data-target="#home-carousel" data-slide-to="1"></li>
+            </ol>
+            <div class="carousel-inner" role="listbox">
+                <div class="item active">
+                    <img src="templates/gbs/images/slides/doom.jpg" alt="">
+                </div>
+                <div class="item">
+                    <img src="templates/gbs/images/slides/mirrorsedge.jpg" alt="">
+                </div>
+            </div>
+
+        </div>
+    </div>
+@stop
+
+@section('content')
+    <div class="container">
+        <br>
+        <div class="row text-center platforms">
+            <div class="col-md-4 form-group">
+                <h3>PC</h3>
+            </div>
+            <div class="col-md-4 form-group">
+                <h3>PS 4</h3>
+            </div> 
+            <div class="col-md-4 form-group">
+                <h3>XBOX</h3>
+            </div>
+        </div>
+
+    </div>
+@stop
